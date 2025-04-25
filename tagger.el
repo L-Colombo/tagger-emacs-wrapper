626e5d60a8cf932ead66b1a24b2e5c4a3b24a4d9
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-04-25 22:08:29 +0200

    add licensing

--- tagger.el
+++ tagger.el
@@ -7,7 +7,26 @@
 ;; Keywords: Org-Mode, Tagger
 ;; URL: https://github.com/L-Colombo/tagger-emacs-wrapper
 
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;; This packages provides wrappers around the "tagger" command line tool
 ;; (https://github.com/L-Colombo/tagger) The aim of "tagger" is to provide
 ;; an easy and faster way to interact (list, search and refile) with tags in your .org files.
